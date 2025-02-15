a39b330af7f7701f004bda8dcb4cdb26a6d1d3eb
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-02-15 12:58:24 +0300

    Menu
    On line 9 to 18 a menuPanel was created with the JMenuBar.

--- Hospital.ui/src/main/java/hospital/ui/MenuPanel.java
+++ Hospital.ui/src/main/java/hospital/ui/MenuPanel.java
@@ -1,11 +1,21 @@
 package hospital.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuPanel {
     public static JPanel createMenuPanel(UIComponents app){
 
-        
+        JPanel menuPanel = new JPanel();
+        menuPanel.setBackground(new Color(59, 25, 25));
+        menuPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBackground(new Color(59, 25, 25));
+
+        JMenu menu = new JMenu("MENU");
+        menu.setForeground(Color.WHITE);
+        menu.setFont(new Font("Arial", Font.BOLD, 16));
     }
 
 }
