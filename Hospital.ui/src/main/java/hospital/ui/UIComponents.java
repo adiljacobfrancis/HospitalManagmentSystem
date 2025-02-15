9320d0c27f5b90ec435399d2f62a59648516b69b
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-02-15 11:59:47 +0300

    CardLayout
    on Line 21 to 33 three cards were added to the card panel.

--- Hospital.ui/src/main/java/hospital/ui/UIComponents.java
+++ Hospital.ui/src/main/java/hospital/ui/UIComponents.java
@@ -18,5 +18,18 @@ public class UIComponents {
 
         JPanel menuPanel = MenuPanel.createMenuPanel(this);
         mainFrame.add(menuPanel, BorderLayout.NORTH);
+
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
+        cardPanel.add(Panels.createHomePanel(), "Home");
+        cardPanel.add(Panels.createPatientPanel(), "Patient");
+        cardPanel.add(Panels.createDoctorPanel(), "Doctor");
+        cardPanel.add(Panels.createAppointmentPanel(), "Appointment");
+        cardPanel.add(Panels.createViewPanel(), "View");
+
+        mainFrame.add(cardPanel, BorderLayout.CENTER);
+        cardLayout.show(cardPanel, "Home");
+        mainFrame.setVisible(true);
     }
 }
