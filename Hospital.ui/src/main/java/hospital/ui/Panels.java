20711e5ba1b32c922b129bd9646432e0827a4a8e
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-02-15 12:04:43 +0300

    CreateHomePanel
    on line 8 to 19 a createPanel method was created which included a label with title WELCOME TOH HOSPITAL MANAGEMENT SYSTMEM and was center to center.

--- Hospital.ui/src/main/java/hospital/ui/Panels.java
+++ Hospital.ui/src/main/java/hospital/ui/Panels.java
@@ -1,5 +1,20 @@
 package hospital.ui;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Panels {
-    
+
+    public static JPanel createHomePanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBackground(Color.LIGHT_GRAY);
+        panel.setBorder(BorderFactory.createLineBorder(new Color(59, 25, 25), 20));
+
+
+        JLabel label = new JLabel("<html><div style='text-align: center;'>WELCOME TO<br>HOSPITAL MANAGEMENT SYSTEM</div></html>", SwingConstants.CENTER);
+        label.setFont(new Font("Serif", Font.BOLD, 40));
+
+        panel.add(label, BorderLayout.CENTER);
+        return panel;
+    }
 }
