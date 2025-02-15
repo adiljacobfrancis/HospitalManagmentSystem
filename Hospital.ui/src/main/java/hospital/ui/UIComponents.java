ac5372a13da1d07e10772ad720e6b94cc0090009
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-02-15 11:51:33 +0300

    Instance Variables
    on line 7 to 9 instance variable were created.

--- Hospital.ui/src/main/java/hospital/ui/UIComponents.java
+++ Hospital.ui/src/main/java/hospital/ui/UIComponents.java
@@ -1,4 +1,10 @@
 package hospital.ui;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class UIComponents {
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
+    private JFrame mainFrame;
 }
